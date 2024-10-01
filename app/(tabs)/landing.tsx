1cffaaeb702b6d95349a0742abba202e6d858e10
ShakishnaviM <shakishnavi27@gmail.com>
2024-10-01 11:53:12 +0530

    add routes

---
+++ app/(tabs)/landing.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+export default function Landing() {
+  return (
+    <div>
+      <Text> Landing page</Text>
+    </div>
+  )
+}
---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Redirect } from 'expo-router'
+import React from 'react'
+import { Text } from 'react-native'
+
+export default function Index() {
+  return (
+    <Text>
+        Index File
+    </Text>
+  )
+}
